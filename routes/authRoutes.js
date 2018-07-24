b69814a79030d8fac0c69cdc0ac2b574a94c4891
Anthony Mansur <amansur99@gmail.com>
2018-07-24 19:03:25 -0400

    updating

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ module.exports = app => {
 
   app.get(
     "/auth/facebook",
-    passport.authenticate("facebook", { scope: ["profile", "email"] })
+    passport.authenticate("facebook", { scope: ["email"] })
   );
 
   app.get(
